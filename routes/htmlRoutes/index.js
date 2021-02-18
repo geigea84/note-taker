4f550e51026c51e1b3263cebf3118490b708b9e2
Geiger <geiga@shopbop.com>
2021-02-18 12:09:50 -0600

    updated comments

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,6 +3,11 @@ const path = require("path");
 
 //get index.html
 router.get("/", (req, res) => {
+    //http://expressjs.com/en/api.html#res.sendFile
+    //transfer the file at the given path
+    //To create a new directory in your index.js file, insert 
+    //__dirname as the first argument to path.join() and the 
+    //name of the new directory as the second
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express    = require("express");
-const apiRoutes  = require("./routes/apiRoutes");
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
-const app        = express();
-const PORT       = process.env.PORT || 3002;
+const app = express();
+const PORT = process.env.PORT || 3002;
 
 //parse incoming string or array data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //parse incoming JSON data
 app.use(express.json());
